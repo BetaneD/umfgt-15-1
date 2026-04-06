2d3f1d18a0c66ff642ce6072dba60ad1ecf4d72f
BetaneD <Betane2.Daudi@live.uwe.ac.uk>
2026-04-06 01:51:05 +0100

    shows how to create a function

---
+++ Coursework/Lab 5/Task 1/The Greeting Module.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Your Task:
+
+     b.& Write a function called print_welcome_message()
+       that takes no arguments and returnsnothing (void).
+     b.& It should print:
+       ****************************************
+       *     ENGINEERING CALCULATOR v1.0      *
+       *    System Ready. Awaiting Input.     *
+       ****************************************
+     b.& Call this function from main() to test it.
+
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void  print_welcome_message() {
+
+    printf("ENGINEERING CALCULATOR v1.0\n");
+    printf("System Ready. Awaiting Input.\n\n");
+}
+
+
+int main() {
+
+    print_welcome_message();
+
+
+    return 0;
+}
