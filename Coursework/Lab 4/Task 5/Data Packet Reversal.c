e3d390a586759bfbe464660d35be46675223ad4b
BetaneD <Betane2.Daudi@live.uwe.ac.uk>
2026-04-06 01:43:00 +0100

    using a for loop to reverse an arrays elements

--- Coursework/Lab 4/Task 5/Data Packet Reversal.c
+++ Coursework/Lab 4/Task 5/Data Packet Reversal.c
@@ -16,7 +16,7 @@ int main(){
     char word[6] ={'E', 'N', 'A', 'T', 'E','B'};
 
     for (int i = 5 ; i >=0 ; i--){
-        printf("%c",word[i]);
+        printf("%c", word[i]);
     }
 
     return 0;
