715fc1dc3d1d2527f1ac75837e2930026e38b9e2
BetaneD <Betane2.Daudi@live.uwe.ac.uk>
2026-04-22 04:56:13 +0100

    I have edited my header file to include a function that would allow me to calculate the peak to peak amplitude of each phase, I struggled with making the loop as it instead of giving me the mas or min would return 0 but after looping at previous codes done in lessons realized that my starting variable was inside the loop instead of outside.

--- Assignment/Code/practice_codes/csv_read/Waveform_math.h
+++ Assignment/Code/practice_codes/csv_read/Waveform_math.h
@@ -33,7 +33,6 @@ double rms_B (int rows,Waveform *data) {
     return rms;
 }
 double rms_C (int rows,Waveform *data) {
-
     double rms = 0.0;
 
     for (int j = 0; j < rows; j++) {
@@ -44,7 +43,7 @@ double rms_C (int rows,Waveform *data) {
         rms = sqrt(rms /rows);
     }
     return rms;
-
+}
 void tolerance_check(int rows, int voltage, int tolerance, double rms) {
 
     int vmax = voltage + (voltage/tolerance);
@@ -57,6 +56,42 @@ void tolerance_check(int rows, int voltage, int tolerance, double rms) {
             printf("\n\n the rms has not met 230V nominal");
 
         }
+}
+
+void amplitude_A (int rows,Waveform *data) {
+
+    double max = 0.0;
+    double min = 0.0;
+
+    for (int j = 0; j < rows; j++) {
+        max = (max >= data[j].phase_A)? max: data[j].phase_A;
+        min = (min <= data[j].phase_A)? min : data[j].phase_A;
+    }
+    printf("\n\n the peak-to-peak amplitude is %.2f", max - min);
+}
+
+void amplitude_B (int rows,Waveform *data) {
+
+    double max = 0.0;
+    double min = 0.0;
+
+    for (int j = 0; j < rows; j++) {
+        max = (max >= data[j].phase_B)? max: data[j].phase_B;
+        min = (min <= data[j].phase_B)? min : data[j].phase_B;
+    }
+    printf("\n\n the peak-to-peak amplitude is %.2f", max - min);
+}
+
+void amplitude_C (int rows,Waveform *data) {
+
+    double max = 0.0;
+    double min = 0.0;
+
+    for (int j = 0; j < rows; j++) {
+        max = (max >= data[j].phase_C)? max: data[j].phase_C;
+        min = (min <= data[j].phase_C)? min : data[j].phase_C;
+    }
+    printf("\n\n the peak-to-peak amplitude is %.2f", max - min);
 }
 
 #endif //UMFGT_15_1_WAVEFORM_MATH_H
