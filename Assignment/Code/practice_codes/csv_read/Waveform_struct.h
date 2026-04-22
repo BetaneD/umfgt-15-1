ba8aea5c61c1e86eaa0845cbbc58b6d0a88e8250
BetaneD <Betane2.Daudi@live.uwe.ac.uk>
2026-04-22 02:14:15 +0100

    I have written code for the struct that would group each row of the cvs file into one place. It has been written as a header file which wa requested in the brief, I should now create a malloc that would be able to store a struct of each line which i could then use pointers to populate it

---
+++ Assignment/Code/practice_codes/csv_read/Waveform_struct.h
@@ -0,0 +1,19 @@
+//
+// Created by Fatima Daudi on 21/04/2026.
+//
+
+#ifndef WAVEFORM_STRUCT_H
+#define WAVEFORM_STRUCT_H
+typedef struct {
+
+    float timestamp;
+    float phase_A;
+    float phase_B;
+    float phase_C;
+    float line_C;
+    float frequency;
+    float power_f;
+    float thd;
+}Waveform;
+
+#endif //UMFGT_15_1_WAVEFORM_STRUCT_H
