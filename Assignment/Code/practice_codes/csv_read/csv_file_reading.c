8ceb10d1cca73ad40011c9a119563b27e36f9f01
BetaneD <Betane2.Daudi@live.uwe.ac.uk>
2026-04-22 02:24:45 +0100

    Using the waveform_struct header file I have created I created a malloc that will be able to allocate memory depending on the number of rows counted and the size of the struct allowing for easy customization.I have edited my previous code so that every time a comma is detected it will store the value as a number in the struct accessed using a header file.

--- Assignment/Code/practice_codes/csv_read/csv_file_reading.c
+++ Assignment/Code/practice_codes/csv_read/csv_file_reading.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include "Waveform_struct.h"
 
 // Driver code
 int main(void)
 {
+int rows = 0;
+    int i = 0;
+
     FILE *fptr = fopen("power_quality_log.csv", "r");
 
     if (fptr==NULL) {
@@ -18,42 +23,70 @@ int main(void)
 
     fgets(line,sizeof(line), fptr);
 
+    while (fgets(line, sizeof(line), fptr) != NULL) {
+        rows++;
+    }
+    printf("Number of rows: %d\n\n", rows);
+
+    Waveform *data = malloc(rows * sizeof(Waveform));
+
+    if (data == NULL) {
+        printf("Error allocating memory: Check malloc\n");
+        fclose(fptr);
+        return 1;
+    }
+
+    rewind(fptr);
+    fgets(line, sizeof(line), fptr);
+
     while(fgets(line, sizeof(line), fptr) != NULL) {
 
         char *token;
 
         token = strtok(line,",");
-        float Time = atof(token);
+          if (token) data[i].timestamp = atof(token);
 
         token = strtok(NULL,",");
-        float phase_A = atof(token);
+          if (token) data[i].phase_A = atof(token);
 
         token = strtok(NULL,",");
-        float phase_B = atof(token);
+          if (token) data[i].phase_B = atof(token);
 
         token = strtok(NULL,",");
-        float phase_C = atof(token);
+          if (token) data[i].phase_C = atof(token);
 
         token = strtok(NULL,",");
-        float line_C = atof(token);
+          if (token) data[i].line_C = atof(token);
 
         token = strtok(NULL,",");
-        float frequency = atof(token);
+          if (token) data[i].frequency = atof(token);
 
         token = strtok(NULL,",");
-        float power_f = atof(token);
+          if (token) data[i].power_f = atof(token);
 
         token = strtok(NULL,",");
-        float thd = atof(token);
+          if (token) data[i].thd = atof(token);
 
-
-        printf("Timestamp:\t %.2f, phase_A:\t %.2f, phase_B:\t %.2f, phase_C:\t %.2f, "
-                "line_C:\t %.2f, frequency:\t %.2f, power_f:\t %.2f, thd:\t %.2f\n",
-               Time,phase_A,phase_B,phase_C,line_C,frequency,power_f,thd);
-
+    i++;
     }
 
-    // Close the file
+  for (int j = 0; j < rows; j++) {
+    printf("Timestamp: %.2f | phase_A: %.2f | phase_B: %.2f | phase_C: %.2f | Line_C: %.2f | Frequency: %.2f | Power_F: %.2f | THD: %.2f\n",
+           data[j].timestamp,
+           data[j].phase_A,
+           data[j].phase_B,
+           data[j].phase_C,
+           data[j].line_C,
+           data[j].frequency,
+           data[j].power_f,
+           data[j].thd
+           );
+  }
+
+
+  free(data);
+
+
     fclose(fptr);
 
     return 0;
